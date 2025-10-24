89c9e3f2847f3d2064a9f29e4d663342eb1e9380
Ferrum58 <sicimalpdemir@gmail.com>
2025-10-24 23:48:34 +0200

    added the X-O-X method

--- ticTacToeGUI.java
+++ ticTacToeGUI.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class ticTacToeGUI extends JFrame {
     private JButton[][] buttons = new JButton[3][3];
     private String[][] board = new String[3][3];
-    
+    private String currentPlayer = "X"
+;    
     public ticTacToeGUI() {
         setTitle("Tic-Tac-Toe");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,8 +17,35 @@ public class ticTacToeGUI extends JFrame {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = " ";
                 buttons[i][j] = new JButton(board[i][j]);
-                add(buttons[i][j]);
+                buttons[i][j].setFont(new Font("Arial", Font.BOLD, 60));
 
+                //final variables for lambda
+                final int row = i;
+                final int col = j;
+                //click handler
+                buttons[i][j].addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        handleClick(row, col);
+                    }
+                });
+                add(buttons[i][j]);
+            }
+        }
+
+        setLocationRelativeTo(null);
+    }
+    
+    private void handleClick(int row, int col) {
+
+        if (board[row][col].equals(" ")) {
+
+            board[row][col] = currentPlayer;
+            buttons[row][col].setText(currentPlayer);
+
+            if (currentPlayer.equals("X")) {
+                currentPlayer = "O";
+            } else {
+                currentPlayer = "X";
             }
         }
     }
