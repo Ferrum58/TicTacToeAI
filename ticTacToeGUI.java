4432e762baf336530d8cc30edd21005127fb7f51
Ferrum58 <sicimalpdemir@gmail.com>
2025-10-26 01:24:23 +0200

    i think the gui is finished

--- ticTacToeGUI.java
+++ ticTacToeGUI.java
@@ -5,7 +5,8 @@ import java.awt.event.*;
 public class ticTacToeGUI extends JFrame {
     private JButton[][] buttons = new JButton[3][3];
     private String[][] board = new String[3][3];
-    private String currentPlayer = "X"
+    private String currentPlayer = "X";
+    private boolean gameOver = false;
 ;    
     public ticTacToeGUI() {
         setTitle("Tic-Tac-Toe");
@@ -37,16 +38,70 @@ public class ticTacToeGUI extends JFrame {
     
     private void handleClick(int row, int col) {
 
-        if (board[row][col].equals(" ")) {
-
+        if (board[row][col].equals(" ") && !gameOver) {
             board[row][col] = currentPlayer;
             buttons[row][col].setText(currentPlayer);
 
+            if (checkWinner(currentPlayer)) {
+                JOptionPane.showMessageDialog(this, currentPlayer + " WINS!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+                gameOver = true;
+                return;
+            }
+
+            if (checkDraw()) {
+                JOptionPane.showMessageDialog(this, "It's a DRAW!", JOptionPane.INFORMATION_MESSAGE);
+                gameOver = true;
+                return;
+            }
             if (currentPlayer.equals("X")) {
                 currentPlayer = "O";
             } else {
                 currentPlayer = "X";
             }
         }
+    }
+
+    private boolean checkWinner(String player) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals(player) &&
+                board[i][1].equals(player) &&
+                board[i][2].equals(player)) {
+                return true;
+                }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i].equals(player) &&
+                board[1][i].equals(player) &&
+                board[2][i].equals(player)) {
+                    return true;
+                }
+        }
+
+        if (board[0][0].equals(player) &&
+            board[1][1].equals(player) &&
+            board[2][2].equals(player)) {
+            return true;
+        }
+
+        if (board[0][2].equals(player) &&
+            board[1][1].equals(player) &&
+            board[2][0].equals(player)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkDraw() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j].equals(" ")) {
+                    return false; // found empty space (not a draw)
+                }
+            }
+        }
+        return true;
     }
 }
